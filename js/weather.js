2196d92c238a0b84e13ab286a2c532a71ba34757
Hamza Khan <khanh39@mcmaster.ca>
2017-01-22 15:39:37 -0500

    implemented geolocation api for user's browser

---
+++ js/weather.js
@@ -0,0 +1,7 @@
+if("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        loadWeather (position.coords.latitude + ',' + position.coords.longitude);
+    });
+} else {
+    loadWeather("Toronto, CA", "");
+}
